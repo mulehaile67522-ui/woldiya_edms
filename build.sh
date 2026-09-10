92476c8920a541da824f680b584761432f58fd6a
Woldiya EDMS <woldiya.edms@gmail.com>
2026-09-10 01:19:47 -0700

    Fix build.sh for Render deploy

--- build.sh
+++ build.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
 python manage.py collectstatic --no-input
-
-# Run database migrations
-python manage.py migrate
-
-# Seed initial categories
-if [ -f seed_categories.py ]; then
-    python seed_categories.py
-fi
+python manage.py migrate --run-syncdb
+python seed_categories.py || true
